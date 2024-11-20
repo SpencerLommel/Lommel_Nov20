14ee70a53e13c14acb84b89e62f31c3c25340b1d
Spencer Lommel <spencer.lommel@ndsu.edu>
2024-11-20 14:45:03 -0600

    Add student.cs

---
+++ Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace Lommel_nov20.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastDescription { get; set; }
+    }
+}
